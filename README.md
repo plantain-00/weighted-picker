557c4c1c1ac5606c3f3919f1677aea968178c2ac
york yao <yaoao12306@outlook.com>
2017-01-26 19:58:51 +0800

    initial

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
+[![Dependency Status](https://david-dm.org/plantain-00/weighted-picker.svg)](https://david-dm.org/plantain-00/weighted-picker)
+[![devDependency Status](https://david-dm.org/plantain-00/weighted-picker/dev-status.svg)](https://david-dm.org/plantain-00/weighted-picker#info=devDependencies)
+[![Build Status](https://travis-ci.org/plantain-00/weighted-picker.svg?branch=master)](https://travis-ci.org/plantain-00/weighted-picker)
+[![npm version](https://badge.fury.io/js/weighted-picker.svg)](https://badge.fury.io/js/weighted-picker)
+[![Downloads](https://img.shields.io/npm/dm/weighted-picker.svg)](https://www.npmjs.com/package/weighted-picker)
+
 # weighted-picker
 A library to pick a random item from weighted array.
+
+#### install
+
+`npm i weighted-picker`
+
+#### usage
+
+```ts
+import { WeightedPicker } from "weighted-picker";
+
+const items = [
+    { name: "a", weight: 1 },
+    { name: "b", weight: 2 },
+    { name: "c", weight: 3 },
+    { name: "d", weight: 4 },
+];
+const picker = new WeightedPicker(items.length, index => items[index].weight);
+picker.pickOne();
+picker.pickOne();
+picker.pickOne();
+Ï```
+
+```ts
+import { WeightedPicker } from "weighted-picker";
+
+const items = ["a", "b", "c", "d"];
+const weights = [1, 2, 3, 4];
+const picker = new WeightedPicker(items.length, index => weights[index]);
+picker.pickOne();
+picker.pickOne();
+picker.pickOne();
+```
---
+++ index.ts
@@ -0,0 +1,49 @@
+export class WeightedPicker {
+    private values: number[] = [];
+    constructor(private count: number, getWeight: (index: number) => number) {
+        if (typeof count !== "number" || count < 0 || isNaN(count)) {
+            this.count = 0;
+        }
+
+        for (let i = 0; i < this.count; i++) {
+            let weight = getWeight(i);
+            if (typeof weight !== "number" || weight < 0 || isNaN(weight)) {
+                weight = 0;
+            }
+            if (i === 0) {
+                this.values.push(weight);
+            } else {
+                this.values.push(weight + this.values[this.values.length - 1]);
+            }
+        }
+    }
+    binaryPick(value: number) {
+        if (value < this.values[0]) {
+            return 0;
+        }
+        let left = 0;
+        let right = this.values.length - 1;
+        while (left < right) {
+            const middle = Math.floor((left + right) / 2);
+            if (value < this.values[middle]) {
+                right = middle - 1;
+            } else if (value >= this.values[middle + 1]) {
+                left = middle + 1;
+            } else {
+                return middle + 1;
+            }
+        }
+        return right + 1;
+    }
+    pickOne() {
+        if (this.count === 0) {
+            return -1;
+        }
+        if (this.count === 1) {
+            return 0;
+        }
+        const randomValue = Math.random() * this.values[this.values.length - 1];
+
+        return this.binaryPick(randomValue);
+    }
+}
