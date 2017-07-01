4d77dd0f0aac9ca9f70aeeddff3c8922968fd449
york yao <yaoao12306@outlook.com>
2017-07-02 00:14:34 +0800

    better unit test

--- spec/indexSpec.ts
+++ spec/indexSpec.ts
@@ -1,20 +1,16 @@
 import { WeightedPicker } from "../index";
 
-describe("all", () => {
+it("", () => {
     const items = ["a", "b", "c", "d"];
     const weights = [1, 2, 3, 4];
     const picker = new WeightedPicker(items.length, index => weights[index]);
-    it("should work", () => {
-        expect(picker.binaryPick(0)).toEqual(0);
-        expect(picker.binaryPick(0.99)).toEqual(0);
-        expect(picker.binaryPick(1)).toEqual(1);
-        expect(picker.binaryPick(2.99)).toEqual(1);
-        expect(picker.binaryPick(3)).toEqual(2);
-        expect(picker.binaryPick(5.99)).toEqual(2);
-        expect(picker.binaryPick(6)).toEqual(3);
-        expect(picker.binaryPick(9.99)).toEqual(3);
-        picker.pickOne();
-        picker.pickOne();
-        picker.pickOne();
-    });
+
+    expect(picker.binaryPick(0)).toEqual(0);
+    expect(picker.binaryPick(0.99)).toEqual(0);
+    expect(picker.binaryPick(1)).toEqual(1);
+    expect(picker.binaryPick(2.99)).toEqual(1);
+    expect(picker.binaryPick(3)).toEqual(2);
+    expect(picker.binaryPick(5.99)).toEqual(2);
+    expect(picker.binaryPick(6)).toEqual(3);
+    expect(picker.binaryPick(9.99)).toEqual(3);
 });
