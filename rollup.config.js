db559e58d6b18ed0ffc89ebbf5fd16bbd796cc72
york yao <yaoao12306@outlook.com>
2017-09-22 15:37:45 +0800

    update rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -2,9 +2,11 @@ import uglify from 'rollup-plugin-uglify'
 import resolve from 'rollup-plugin-node-resolve'
 
 export default {
-  entry: 'dist/browser/index.js',
-  dest: 'dist/weighted-picker.min.js',
-  format: 'umd',
-  moduleName: 'WeightedPicker',
-  plugins: [resolve(), uglify()]
+  input: 'dist/browser/index.js',
+  name: 'WeightedPicker',
+  plugins: [resolve(), uglify()],
+  output: {
+    file: 'dist/weighted-picker.min.js',
+    format: 'umd'
+  }
 }
