80ad1c7d2598fb87bc6f865c9662589d328a5a70
york yao <yaoao12306@outlook.com>
2018-10-30 23:02:20 +0800

    chore: add type-coverage check

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,7 @@ import resolve from 'rollup-plugin-node-resolve'
 export default {
   input: 'dist/browser/index.js',
   name: 'WeightedPicker',
-  plugins: [resolve(), uglify()],
+  plugins: [resolve({ browser: true }), uglify()],
   output: {
     file: 'dist/weighted-picker.min.js',
     format: 'umd'
