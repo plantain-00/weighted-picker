abac2048a0b47614e54568f388f0e891c6e30111
york yao <yaoao12306@outlook.com>
2018-12-30 09:01:01 +0800

    chore: update dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -3,9 +3,9 @@ import resolve from 'rollup-plugin-node-resolve'
 
 export default {
   input: 'dist/browser/index.js',
-  name: 'WeightedPicker',
   plugins: [resolve({ browser: true }), uglify()],
   output: {
+    name: 'WeightedPicker',
     file: 'dist/weighted-picker.min.js',
     format: 'umd'
   }
